99c719c9157f3185e80a4833bcc051d6d8cbc2a1
DK\MVO <mvo@sitecore.net>
2017-01-30 11:45:57 +0200

    fix the issue related to displaying inner exceptions on mvc renderings

---
+++ src/Sitecore.Support.145188/PageModeRenderingErrorStrategy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Mvc.Diagnostics;
+using Sitecore.Mvc.Presentation;
+using System.IO;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.Mvc.Pipelines.Response.RenderRendering
+{
+    public class PageModeRenderingErrorStrategy : Sitecore.Mvc.Pipelines.Response.RenderRendering.PageModeRenderingErrorStrategy, Sitecore.Mvc.Pipelines.Response.RenderRendering.IRendererErrorStrategy
+    {
+        public bool HandleError(Renderer renderer, Exception ex, TextWriter writer)
+        {
+            Assert.IsNotNull(renderer, "renderer");
+            Assert.IsNotNull(ex, "ex");
+            Assert.IsNotNull(writer, "writer");
+            ExceptionWrapper exw = ex as ExceptionWrapper;
+            if (exw != null)
+            {
+                ex = exw.Exception;
+            }
+            return base.HandleError(renderer, ex, writer);
+        }
+    }
+}
---
+++ src/Sitecore.Support.145188/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.145188")]
+[assembly: AssemblyProduct("Sitecore.Support.145188")]
+[assembly: ComVisible(false)]
